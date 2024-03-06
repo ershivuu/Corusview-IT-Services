135bf843d7c7787e08fec99623fa964e21020678
rahul.gandhi@cviewsurvey.com <rahul.gandhi@cviewsurvey.com>
2024-03-06 14:28:00 +0530

    initial commit

--- src/components/Headers/Nav.jsx
+++ src/components/Headers/Nav.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import "./Nav.css"
+import "./Nav.css";
+import logo from "../../assets/logos/corusview-logo (1).png";
+import hamburger from "../../assets/logos/hamburger.png"
 
 function Nav() {
   return (
     <>
       <div>
-        <div className="header-body"></div>
+        <div className="header-body">
+          <div className="header-child">
+            <img className="header-logo" src={logo} />
+            <div>
+             <img className="hamburger" src={hamburger}/>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
