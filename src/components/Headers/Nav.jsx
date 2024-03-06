fc28f49becf46b170dda01d48d3703a68e868625
rahul.gandhi@cviewsurvey.com <rahul.gandhi@cviewsurvey.com>
2024-03-06 14:50:50 +0530

    initial commit

--- src/components/Headers/Nav.jsx
+++ src/components/Headers/Nav.jsx
@@ -4,6 +4,7 @@ import logo from "../../assets/logos/corusview-logo (1).png";
 import hamburger from "../../assets/logos/hamburger.png"
 
 function Nav() {
+  
   return (
     <>
       <div>
@@ -12,6 +13,7 @@ function Nav() {
             <img className="header-logo" src={logo} />
             <div>
              <img className="hamburger" src={hamburger}/>
+             
             </div>
           </div>
         </div>
