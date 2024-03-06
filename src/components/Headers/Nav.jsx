4373f339bdf6b11a89a822129dcaa4bb46a97f15
Shivam Gupta <er.shivamsg@gmail.com>
2024-03-06 14:50:05 +0530

    Header code added

--- src/components/Headers/Nav.jsx
+++ src/components/Headers/Nav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Nav.css";
 import logo from "../../assets/logos/corusview-logo (1).png";
-import hamburger from "../../assets/logos/hamburger.png"
+import hamburger from "../../assets/logos/hamburger.png";
 
 function Nav() {
   return (
@@ -11,7 +11,33 @@ function Nav() {
           <div className="header-child">
             <img className="header-logo" src={logo} />
             <div>
-             <img className="hamburger" src={hamburger}/>
+              <div className="dropdown">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <img className="hamburger" src={hamburger} />
+                </button>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
